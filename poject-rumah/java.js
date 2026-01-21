6f5bf33552ce025b76cc1563994d3eed4058ff00
azki0102-afk <azki0102@student.abudzar.sch.id>
2026-01-21 13:27:00 +0700

    proyek-3

---
+++ poject-rumah/java.js
@@ -0,0 +1,9 @@
+// script.js
+document.addEventListener('DOMContentLoaded', function() {
+    const garudaImg = document.getElementById('garuda-img');
+    
+    // Tambahkan kelas CSS yang memicu animasi
+    if (garudaImg) {
+        garudaImg.classList.add('animate-start');
+    }
+});
